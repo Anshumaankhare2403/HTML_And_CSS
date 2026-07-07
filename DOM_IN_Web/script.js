ad9ea62eed276f629b2e9b78d4004e6360d95169
Anshumaankhare2403 <anshumaankhare2403@gmail.com>
2026-07-07 14:08:57 +0530

    DOM

--- DOM_IN_Web/script.js
+++ DOM_IN_Web/script.js
@@ -1,15 +1,60 @@
-const dom = document.getElementsByClassName("Row");
+// // const dom = document.getElementsByClassName("Row");
 
-for (let i = 0; i < dom.length; i++) {
-    dom[i].style.background = "green";
+// // for (let i = 0; i < dom.length; i++) {
+// //     dom[i].style.background = "green";
+// // }
+
+// // let count = document.getElementById("count");
+// // let a = 0;
+
+// // function add() {
+// //     a++;
+// //     count.textContent = a;
+// //     console.log(a)
+// // }
+
+// const div  = document.querySelector(".Row");
+// div.innerHTML="Anshumaan";
+// // for (let i = 0; i < 5; i++) {
+// // }
+
+// Login Forms
+// let name = document.getElementById("name");
+// let email = document.getElementById("email");
+// let password = document.getElementById("password");
+
+// console.log(name.value);
+// console.log(email.value);
+// console.log(password.value);
+
+// const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+// const user = [];
+// function signin(event) {
+
+//     event.preventDefault();
+//     if (!emailRegex.test(email.value)) {
+//         console.log("Invalid email");
+//         return;
+//     }
+//     if(password.value.length < 8){
+//         console.log("password length");
+//         return;
+//     }
+    
+
+//     const data = {
+//         name: name.value,
+//         email: email.value,
+//         password: password.value
+//     };
+//     user.push(data);
+//     console.log(user)
+// }
+
+
+let change = document.getElementById("body");
+
+function colorchange(){
+    change.style.backgroundColor="green";
 }
-
-let count = document.getElementById("count");
-let a = 0;
-
-function add() {
-    a++;
-    count.textContent = a;
-    console.log(a)
-}
-
